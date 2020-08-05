516ef095a1c674aac7fbdc6444eb08cbbbc18fa5
Annanda Sousa <annanda.sousa@insight-centre.org>
2020-08-05 12:02:35 +0100

    Adding more feature type configuration.

--- build_dataset_gaze.py
+++ build_dataset_gaze.py
@@ -1,23 +1,16 @@
 import pathlib
 import os.path
+import glob
 
 import pandas as pd
 
 main_folder = pathlib.Path(__file__).parent.absolute()
 dataset_video_folder = os.path.join(main_folder, 'dataset', 'video')
 feature_type_dict = {
-    'gaze': {'start': 'gaze_0_x', 'end': 'gaze_1_z'}
+    'gaze': {'start': 'gaze_0_x', 'end': 'gaze_1_z'},
+    '2d_eye_landmark': {'start': 'eye_lmk_x_0', 'end': 'eye_lmk_y_55'},
+    '3d_eye_landmark': {'start': 'eye_lmk_X_0', 'end': 'eye_lmk_Z_55'},
 }
-
-
-def extract_gaze(extrated_csv):
-    prefix = extrated_csv.split('/')[-1]
-    df_raw = pd.read_csv(extrated_csv, sep=r'\s*,\s*',
-                         header=0, engine='python')
-    df_initial = df_raw[['frame', 'timestamp']]
-    gaze_df = df_raw.loc[:, 'gaze_0_x':'gaze_1_z']
-    final = pd.concat([df_initial, gaze_df], axis=1)
-    final.to_csv(f'{dataset_video_folder}/gaze/{prefix}', index=False)
 
 
 def extract_features(extrated_csv, feature_type):
@@ -29,16 +22,16 @@ def extract_features(extrated_csv, feature_type):
     column_to_extract_end = feature_type_dict[feature_type]['end']
     gaze_df = df_raw.loc[:, column_to_extract_start:column_to_extract_end]
     final = pd.concat([df_initial, gaze_df], axis=1)
-    final.to_csv(f'{dataset_video_folder}/gaze/{prefix}', index=False)
+    final.to_csv(f'{dataset_video_folder}/{feature_type}/{prefix}', index=False)
 
 
-def extract_3d_eye_landmark():
-    pass
+def extract_features_all_files(feature_type):
+    # files = []
+    files = glob.glob(f'{main_folder}/processed_csv/*.csv')
+    for file in files:
+        extract_features(file, feature_type)
 
 
 if __name__ == '__main__':
-    dataset_path = 'processed_csv/train_3.csv'
-    # extract_gaze(dataset_path)
-    # extract_features(dataset_path, 'gaze_0_x', 'gaze_1_z')
-    feature_type = 'gaze'
-    extract_features(dataset_path, feature_type)
+    feature_type = '2d_eye_landmark'
+    extract_features_all_files(feature_type)
