c2bcfe87c9be086cff8d8a53fd8a114fcee4141a
Annanda Sousa <annanda.sousa@insight-centre.org>
2020-08-10 14:21:15 +0100

    Adding more features.

--- build_dataset.py
+++ build_dataset.py
@@ -4,20 +4,32 @@ import glob
 
 import pandas as pd
 
+from annotation_frametime import round_frametime
+
 main_folder = pathlib.Path(__file__).parent.absolute()
 dataset_video_folder = os.path.join(main_folder, 'dataset', 'video')
 feature_type_dict = {
-    'gaze': {'start': 'gaze_0_x', 'end': 'gaze_1_z'},
+    'gaze': {'start': 'gaze_0_x', 'end': 'gaze_angle_y'},
     '2d_eye_landmark': {'start': 'eye_lmk_x_0', 'end': 'eye_lmk_y_55'},
     '3d_eye_landmark': {'start': 'eye_lmk_X_0', 'end': 'eye_lmk_Z_55'},
+    'head_pose': {'start': 'pose_Tx', 'end': 'pose_Rz'},
+    'face_2d_landmarks': {'start': 'x_0', 'end': 'y_67'},
+    'face_3d_landmarks': {'start': 'X_0', 'end': 'Z_67'},
 }
 
 
 def extract_features(extrated_csv, feature_type):
     prefix = extrated_csv.split('/')[-1]
+    file_name_suffix = prefix.split('.')[0]
     df_raw = pd.read_csv(extrated_csv, sep=r'\s*,\s*',
                          header=0, engine='python')
-    df_initial = df_raw[['frame', 'timestamp']]
+    df_initial = pd.DataFrame()
+    df_timestamp = df_raw[['timestamp']]
+    base = 0.04
+    df_initial = df_initial.assign(
+        frametime=df_timestamp['timestamp'].map(lambda x: round(base * round(x / base), 2))
+    )
+    df_initial['frametime'] = file_name_suffix + '___' + df_initial['frametime'].astype(str)
     column_to_extract_start = feature_type_dict[feature_type]['start']
     column_to_extract_end = feature_type_dict[feature_type]['end']
     gaze_df = df_raw.loc[:, column_to_extract_start:column_to_extract_end]
@@ -33,5 +45,5 @@ def extract_features_all_files(feature_type):
 
 
 if __name__ == '__main__':
-    feature_type = '2d_eye_landmark'
+    feature_type = 'face_3d_landmarks'
     extract_features_all_files(feature_type)
