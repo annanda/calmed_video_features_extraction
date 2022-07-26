f0c9a97d64a6035959707b77c0132ddfa19e8c02
Annanda Sousa <annanda.sousa@insight-centre.org>
2022-07-26 15:14:52 +0100

    More refactoring and including the code to extract all features from a session at once.

--- build_dataset.py
+++ build_dataset.py
@@ -2,17 +2,7 @@ import os.path
 
 import pandas as pd
 
-from setup.conf import DATASET_VIDEO_FOLDER, PROCESSED_VIDEO_CSV_FOLDER
-
-feature_type_dict = {
-    'gaze': {'start': 'gaze_0_x', 'end': 'gaze_angle_y'},
-    '2d_eye_landmark': {'start': 'eye_lmk_x_0', 'end': 'eye_lmk_y_55'},
-    '3d_eye_landmark': {'start': 'eye_lmk_X_0', 'end': 'eye_lmk_Z_55'},
-    'head_pose': {'start': 'pose_Tx', 'end': 'pose_Rz'},
-    'face_2d_landmarks': {'start': 'x_0', 'end': 'y_67'},
-    'face_3d_landmarks': {'start': 'X_0', 'end': 'Z_67'},
-    'AU': {'start': 'AU01_r', 'end': 'AU45_c'}
-}
+from setup.conf import DATASET_VIDEO_FOLDER, PROCESSED_VIDEO_CSV_FOLDER, FEATURE_TYPE_DICT, FEATURES_VIDEO
 
 
 def extract_features(session, feature_type):
@@ -34,29 +24,22 @@ def extract_features(session, feature_type):
             frametime=df_timestamp['timestamp'].map(lambda x: round(base * round(x / base), 2))
         )
         df_initial['frametime'] = name + '___' + df_initial['frametime'].astype(str)
-        column_to_extract_start = feature_type_dict[feature_type]['start']
-        column_to_extract_end = feature_type_dict[feature_type]['end']
+        column_to_extract_start = FEATURE_TYPE_DICT[feature_type]['start']
+        column_to_extract_end = FEATURE_TYPE_DICT[feature_type]['end']
         gaze_df = df_raw.loc[:, column_to_extract_start:column_to_extract_end]
         final = pd.concat([df_initial, gaze_df], axis=1)
         final.to_csv(os.path.join(output_folder, f'{name}.csv'), index=False)
 
 
-# def extract_features_all_files(feature_type):
-#     # files = []
-#     files = glob.glob(f'{MAIN_FOLDER}/processed_csv/*.csv')
-#     for file in files:
-#         extract_features(session, feature_type)
+def extract_all_features_from_session(session):
+    for feature in FEATURES_VIDEO:
+        extract_features(session, feature)
 
 
 if __name__ == '__main__':
-    feature_type = '2d_eye_landmark'
     session_number = 'session_01_01'
-    # extract_features_all_files(feature_type)
+    # To extract all video features from a session use the function below
+    extract_all_features_from_session(session_number)
 
-    features = ['gaze', '2d_eye_landmark', '3d_eye_landmark', 'head_pose', 'face_2d_landmarks', 'face_3d_landmarks',
-                'AU']
-
-    # for feature in features:
-    #     extract_features_all_files(feature)
-
-    extract_features(session_number, feature_type)
+    # To extract one specific feature from a session use the function below
+    # extract_features(session_number, feature)
--- setup/conf.py
+++ setup/conf.py
@@ -10,3 +10,17 @@ processed_video_csv_folder = os.path.join(main_folder, 'processed_csv', 'video')
 MAIN_FOLDER = config('MAIN_FOLDER', default=main_folder)
 DATASET_VIDEO_FOLDER = config('DATASET_VIDEO_FOLDER', default=dataset_video_folder)
 PROCESSED_VIDEO_CSV_FOLDER = config('PROCESSED_VIDEO_CSV_FOLDER', default=processed_video_csv_folder)
+
+# FEATURES VIDEO VALUES
+FEATURE_TYPE_DICT = {
+    'gaze': {'start': 'gaze_0_x', 'end': 'gaze_angle_y'},
+    '2d_eye_landmark': {'start': 'eye_lmk_x_0', 'end': 'eye_lmk_y_55'},
+    '3d_eye_landmark': {'start': 'eye_lmk_X_0', 'end': 'eye_lmk_Z_55'},
+    'head_pose': {'start': 'pose_Tx', 'end': 'pose_Rz'},
+    'face_2d_landmarks': {'start': 'x_0', 'end': 'y_67'},
+    'face_3d_landmarks': {'start': 'X_0', 'end': 'Z_67'},
+    'AU': {'start': 'AU01_r', 'end': 'AU45_c'}
+}
+
+FEATURES_VIDEO = ['gaze', '2d_eye_landmark', '3d_eye_landmark', 'head_pose', 'face_2d_landmarks', 'face_3d_landmarks',
+                  'AU']
