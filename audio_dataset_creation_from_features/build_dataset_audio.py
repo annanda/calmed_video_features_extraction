77eac04fc20c80a8eef3dc1ff1e48bf015e6f43b
Annanda Sousa <annanda.sousa@insight-centre.org>
2022-10-12 09:24:17 +0100

    Generating all audio features type from a given group and session number.

--- audio_dataset_creation_from_features/build_dataset_audio.py
+++ audio_dataset_creation_from_features/build_dataset_audio.py
@@ -5,37 +5,6 @@ from configs.conf import DATASET_AUDIO_FOLDER, OPEN_SMILE_DATASET_FOLDER, SESSIO
     AUDIO_PARAMETER_FEATURES_GROUPS, LLD_PARAMETER_GROUP
 
 
-# def extract_features_audio(session, feature_type):
-#     input_folder = os.path.join(OPEN_SMILE_DATASET_FOLDER, session)
-#     all_session_files = os.listdir(input_folder)
-#     output_folder = os.path.join(DATASET_AUDIO_FOLDER, feature_type, session)
-#     if not os.path.exists(output_folder):
-#         os.makedirs(output_folder)
-#
-#     for file in all_session_files:
-#         name = file.replace('.csv', '')
-#         file_path = os.path.join(input_folder, file)
-#         df_raw = pd.read_csv(file_path, sep=r'\s*,\s*',
-#                              header=0, engine='python')
-#         df_initial = pd.DataFrame()
-#         df_timestamp = df_raw[['timestamp']]
-#         base = 0.04
-#         df_initial = df_initial.assign(
-#             frametime=df_timestamp['timestamp'].map(lambda x: round(base * round(x / base), 2))
-#         )
-#         df_initial['frametime'] = name + '___' + df_initial['frametime'].astype(str)
-#         column_to_extract_start = FEATURE_TYPE_DICT[feature_type]['start']
-#         column_to_extract_end = FEATURE_TYPE_DICT[feature_type]['end']
-#         gaze_df = df_raw.loc[:, column_to_extract_start:column_to_extract_end]
-#         final = pd.concat([df_initial, gaze_df], axis=1)
-#         final.to_csv(os.path.join(output_folder, f'{name}.csv'), index=False)
-
-
-# def extract_all_features_from_session(session):
-#     for feature in FEATURES_VIDEO:
-#         extract_features_audio(session, feature)
-
-
 class AudioDatasetCreator:
     def __init__(self, session, feature_group='all', feature_level='functionals', feature_set='eGeMAPSv02'):
         self.session = session
@@ -57,8 +26,12 @@ class AudioDatasetCreator:
         It saves the datasets into the folder structure (/dataset/audio/feature_level/session) for llds and llds delta
         :return: None
         """
-        self.generate_one_feature_group(self.feature_group)
-        print('hi')
+        if self.feature_group == 'all':
+            feature_groups = LLD_PARAMETER_GROUP.keys()
+        else:
+            feature_groups = [self.feature_group]
+        for feature_group in feature_groups:
+            self.generate_feature_group(feature_group)
 
     def write_csv_dataset(self, df_to_save, session_part, feature_type):
         where_to_save = os.path.join(self.feature_level_output_folders[self.feature_level], feature_type, self.session)
@@ -69,7 +42,7 @@ class AudioDatasetCreator:
             os.path.join(where_to_save, f'{session_part}.csv'),
             index=False)
 
-    def generate_one_feature_group(self, feature_group):
+    def generate_feature_group(self, feature_group):
         """
         To generate one feature type from the current session number.
         Feature groups: frequency, energy_amplitude, spectral_balance, temporal_features
@@ -88,7 +61,7 @@ class AudioDatasetCreator:
             df_initial['frametime'] = session_part + '___' + df_initial['frametime'].astype(str)
 
             features_from_group = LLD_PARAMETER_GROUP[feature_group]
-            for feature_in_group in features_from_group[3:]:
+            for feature_in_group in features_from_group:
                 intervals = AUDIO_PARAMETER_FEATURES_GROUPS[feature_in_group]['intervals']
                 intervals_dfs = []
                 for interval in intervals:
@@ -100,12 +73,15 @@ class AudioDatasetCreator:
                 self.write_csv_dataset(final, session_part, feature_in_group)
 
 
-if __name__ == '__main__':
-    session = 'session_04_01'
-    # If feature level is LLDs or LLDs deltas: run for all features there
-    # If feature level is Functionals: only run for the feature types desired.
+def generate_features_all_sessions():
+    for session in SESSION_PARTS.keys():
+        audio_ds_creator = AudioDatasetCreator(session, feature_group='frequency')
+        audio_ds_creator.generate_features()
 
-    # To extract all audio features from a session use the class below with the default configuration, 
-    # just changing the session number desired
-    audio_dataset_creator = AudioDatasetCreator(session, feature_group='frequency')
+
+if __name__ == '__main__':
+    session_number = 'session_04_02'
+    audio_dataset_creator = AudioDatasetCreator(session_number, feature_group='frequency')
     audio_dataset_creator.generate_features()
+
+    # generate_features_all_sessions()
